67301f84fd643a080e96fde82f3c5c730cf55472
karthik <karthikp8688@gmail.com>
2026-02-22 19:48:31 +0530

    Update Water_jug.py

--- Water_jug.py
+++ Water_jug.py
@@ -1,60 +1,67 @@
-def water_jug_dfs(capacity1, capacity2, target):
-    visited = set()  
-    path = []  
-    def dfs(jug1, jug2):
-        
+from collections import deque
+
+def water_jug_problem(jug1_capacity, jug2_capacity, target):
+    if target > max(jug1_capacity, jug2_capacity):
+        return "No solution possible"
+
+    visited = set()
+    queue = deque()
+    queue.append((0, 0, []))
+
+    while queue:
+        jug1, jug2, path = queue.popleft()
+
         if (jug1, jug2) in visited:
-            return False
-        
-        
+            continue
+
         visited.add((jug1, jug2))
 
-        
-        path.append((jug1, jug2))
-
-        
         if jug1 == target or jug2 == target:
-            return True
+            path.append((jug1, jug2))
+            return path
 
-        
-        if dfs(3, jug2):
-            return True
-        
-        if dfs(jug1, 5):
-            return True
-        
-        if dfs(0, jug2):
-            return True
-        
-        if dfs(jug1, 0):
-            return True
-        
-        if dfs(max(0, jug1 - (5 - jug2)), min(5, jug1 + jug2)):
-            return True
-        
-        if dfs(min(3, jug1 + jug2), max(0, jug2 - (3 - jug1))):
-            return True
+        next_states = []
 
-        
-        path.pop()
-        return False
+        next_states.append((jug1_capacity, jug2))
+        next_states.append((jug1, jug2_capacity))
+        next_states.append((0, jug2))
+        next_states.append((jug1, 0))
 
-    
-    dfs(0, 0)
+        transfer = min(jug1, jug2_capacity - jug2)
+        next_states.append((jug1 - transfer, jug2 + transfer))
 
-    
-    return path
+        transfer = min(jug2, jug1_capacity - jug1)
+        next_states.append((jug1 + transfer, jug2 - transfer))
+
+        for state in next_states:
+            if state not in visited:
+                queue.append((state[0], state[1], path + [(jug1, jug2)]))
+
+    return "No solution found"
 
 
-capacity1 = 4
-capacity2 =  5
-target =  3   
+jug1 = int(input("Enter capacity of Jug 1: "))
+jug2 = int(input("Enter capacity of Jug 2: "))
+target = int(input("Enter target amount: "))
 
-solution = water_jug_dfs(capacity1, capacity2, target)
+solution = water_jug_problem(jug1, jug2, target)
 
-if solution:
-    print("Solution steps:")
+if isinstance(solution, list):
+    print("Solution Steps:")
     for step in solution:
         print(step)
 else:
-    print("No solution found.")
+    print(solution)
+
+
+output :
+Enter capacity of Jug 1: 4
+Enter capacity of Jug 2: 3
+Enter target amount: 2
+
+Solution Steps:
+(0, 0)
+(0, 3)
+(3, 0)
+(3, 3)
+(4, 2)
